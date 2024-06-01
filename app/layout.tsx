798fda94416bc07d35c55bcb048e52ea9c9a44b4
A. Maslarov <nsite2014@gmail.com>
2024-06-01 20:36:02 +0300

    Analytics 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 // import "./globals.css";
 import "./main.scss";
 import { ThemeProvider } from "@/components/theme-provider";
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
