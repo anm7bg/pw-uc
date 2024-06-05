066b599b3c74603494520c2ec00624200e9d2d38
A. Maslarov <nsite2014@gmail.com>
2024-06-05 19:20:06 +0300

    sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://powweb.bg/sitemap.xml',
+    sitemap: 'https://powweb.bg/index-sitemap.xml',
   }
 }
