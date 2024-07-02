9d339d0597befd2ff9d2257c5e4507649f3e230e
A. Maslarov <nsite2014@gmail.com>
2024-07-02 17:04:51 +0300

    app errs3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["sanity.io", "via.placeholder.com"],
-        // domains: ["cdn.sanity.io", "via.placeholder.com"],
+        // domains: ["sanity.io", "via.placeholder.com"],
+        domains: ["cdn.sanity.io", "via.placeholder.com"],
       },
 };
 
--- powweb-blog/sanity.cli.ts
+++ powweb-blog/sanity.cli.ts
@@ -1,6 +1,6 @@
 import { defineCliConfig } from 'sanity/cli'
+import { projectId } from './env';
+import { dataset } from './env';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({ api: { projectId, dataset } })
