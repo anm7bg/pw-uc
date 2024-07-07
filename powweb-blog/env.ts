ef80a213752628036cb4b72832548b8fbea99d90
A. Maslarov <nsite2014@gmail.com>
2024-07-07 16:08:11 +0300

    dataset

--- powweb-blog/env.ts
+++ powweb-blog/env.ts
@@ -3,10 +3,10 @@
 // console.log(process.env.NEXT_PUBLIC_SANITY_DATASET)
 
 export const apiVersion =
-  process.env.SANITY_STUDIO_API_VERSION
+  process.env.SANITY_STUDIO_API_VERSION || '2022-03-07'
 
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET,
+  process.env.SANITY_STUDIO_DATASET || 'production',
   'Missing environment variable: SANITY_DATASET'
 )
 
@@ -14,11 +14,11 @@ export const dataset = assertValue(
 // console.log(process.env.test)
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID,
+  process.env.SANITY_STUDIO_PROJECT_ID || 'n2yj5q5m',
   'Missing environment variable: SANITY_PROJECT_ID'
 )
 
-export const useCdn = true
+export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
