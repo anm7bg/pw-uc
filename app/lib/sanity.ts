c61baec53f099c2c63409829afc1bf0708864d87
A. Maslarov <nsite2014@gmail.com>
2024-07-08 16:58:07 +0300

    v1

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,9 +2,11 @@ import { createClient } from "next-sanity";
 import { projectId } from "@/powweb-blog/env";
 import { apiVersion } from "@/powweb-blog/env";
 import { dataset } from "@/powweb-blog/env";
+import { useCdn } from "@/powweb-blog/env";
 
 export const client = createClient ({
     apiVersion,
     dataset,
     projectId,
+    useCdn
 })
